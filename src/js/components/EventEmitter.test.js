84b03bb6caba7484f35160d229fd78d6331f33ac
Lukashevich Maksim <lukashevich2014@gmail.com>
2019-05-10 13:00:46 +0300

    saving

--- src/js/components/EventEmitter.test.js
+++ src/js/components/EventEmitter.test.js
@@ -6,3 +6,19 @@ test('check', () => {
   emitter.on('e', handler);
   expect(emitter.eventTable).toHaveProperty('e');
 });
+
+test('subscribe', () => {
+  const emitter = new EventEmitter();
+  const fn = jest.fn();
+  emitter.on('event', fn);
+  emitter.emit('event');
+  expect(fn).toHaveBeenCalled();
+});
+
+test('check111', () => {
+  const emitter = new EventEmitter();
+  const fn = jest.fn();
+  const unsub = emitter.on('event', fn);
+  unsub();
+  expect(emitter.eventTable.event).not.toContain(fn);
+});
